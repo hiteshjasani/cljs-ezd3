3f280ad00191ab1ec2aa47bbd750ff38be666ec1
Hitesh Jasani <hitesh.jasani@gmail.com>
2018-05-10 19:29:50 -0400

    Initial project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to ezd3
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/ezd3/core_test.clj
@@ -0,0 +1,7 @@
+(ns ezd3.core-test
+  (:require [clojure.test :refer :all]
+            [ezd3.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
