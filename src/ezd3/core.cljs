f74b9cfb724e2caed31964b40ea9aedc5a770b08
Hitesh Jasani <hitesh.jasani@gmail.com>
2018-05-10 21:14:18 -0400

    d3svg component

--- src/ezd3/core.cljs
+++ src/ezd3/core.cljs
@@ -1,6 +1,34 @@
-(ns ezd3.core)
+(ns ezd3.core
+  (:require [reagent.core :as r]
+            [cljsjs/d3]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn d3svg
+  [width height did-mount-cb]
+  (let [did-mount (fn [elem]
+                    (let [svg (-> js/d3
+                                  (.select (r/dom-node elem))
+                                  (.append "svg")
+                                  (.attr "width" width)
+                                  (.attr "height" height)
+                                  (.style "border" "1px solid black"))]
+                      (did-mount-cb svg)))
+        on-render (fn [width height did-mount-cb]
+                    [:div])]
+    (r/create-class
+     {:display-name        "d3svg"
+      :reagent-render      on-render
+      :component-did-mount did-mount})))
+
+(defn attrs
+  "Apply attribute map to element as attributes"
+  [elem m]
+  (doseq [[k v] m]
+    (.attr elem k v))
+  elem)
+
+(defn styles
+  "Apply attribute map to element as styles"
+  [elem m]
+  (doseq [[k v] m]
+    (.style elem k v))
+  elem)
