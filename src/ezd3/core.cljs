af7211111ad57a3e187f2210e1251916d7ea3d40
Hitesh Jasani <hitesh.jasani@gmail.com>
2018-05-10 21:23:10 -0400

    Allow customizing d3svg component

--- src/ezd3/core.cljs
+++ src/ezd3/core.cljs
@@ -2,23 +2,6 @@
   (:require [reagent.core :as r]
             [cljsjs/d3]))
 
-(defn d3svg
-  [width height did-mount-cb]
-  (let [did-mount (fn [elem]
-                    (let [svg (-> js/d3
-                                  (.select (r/dom-node elem))
-                                  (.append "svg")
-                                  (.attr "width" width)
-                                  (.attr "height" height)
-                                  (.style "border" "1px solid black"))]
-                      (did-mount-cb svg)))
-        on-render (fn [width height did-mount-cb]
-                    [:div])]
-    (r/create-class
-     {:display-name        "d3svg"
-      :reagent-render      on-render
-      :component-did-mount did-mount})))
-
 (defn attrs
   "Apply attribute map to element as attributes"
   [elem m]
@@ -32,3 +15,23 @@
   (doseq [[k v] m]
     (.style elem k v))
   elem)
+
+(def default-attrs {"width" 200 "height" 200})
+(def default-style {"border" "1px solid black"})
+
+(defn d3svg
+  ([did-mount-cb] (d3svg default-attrs default-style did-mount-cb))
+  ([attr-map style-map did-mount-cb]
+   (let [did-mount (fn [elem]
+                     (let [svg (-> js/d3
+                                   (.select (r/dom-node elem))
+                                   (.append "svg")
+                                   (attrs attr-map)
+                                   (styles style-map))]
+                       (did-mount-cb svg)))
+         on-render (fn [width height did-mount-cb]
+                     [:div])]
+     (r/create-class
+      {:display-name        "d3svg"
+       :reagent-render      on-render
+       :component-did-mount did-mount}))))
