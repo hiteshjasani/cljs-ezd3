57b83174df9f5db1dd420ae859ee201de6294293
Hitesh Jasani <hitesh.jasani@gmail.com>
2018-05-12 11:20:26 -0400

    0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.jasani/ezd3 "0.1.0-SNAPSHOT"
+(defproject org.jasani/ezd3 "0.1.0"
   :description "A library to enable easy D3 and Reagent interop"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
