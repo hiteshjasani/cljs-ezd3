36d0cdbb1f42516ae078efc4afa14098cb4fefe7
Hitesh Jasani <hitesh.jasani@gmail.com>
2018-05-28 22:16:00 -0400

    Update license reference

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject org.jasani/ezd3 "0.1.1-SNAPSHOT"
   :description "A library to enable easy D3 and Reagent interop"
   :url "https://github.com/hiteshjasani/cljs-ezd3"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "BSD 2-Clause \"Simplified\" License"
+            :url "https://spdx.org/licenses/BSD-2-Clause.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/clojurescript "1.10.238"]
                  [cljsjs/d3 "4.12.0-0"]
